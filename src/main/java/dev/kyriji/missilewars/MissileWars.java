162d925a18573a7c8673bdf7cf19dcd449595100
Timothy <timothyrwashburn@gmail.com>
2024-12-27 20:28:17 -0800

    new triton stom features

--- src/main/java/dev/kyriji/missilewars/MissileWars.java
+++ src/main/java/dev/kyriji/missilewars/MissileWars.java
@@ -1,53 +1,39 @@
 package dev.kyriji.missilewars;
 
 import dev.kyriji.missilewars.commands.TestCommand;
+import dev.kyriji.tritonstom.TritonStom;
 import dev.kyriji.tritonstom.worlds.TritonWorld;
 import dev.kyriji.tritonstom.worlds.WorldManager;
+import dev.kyriji.tritonstom.worlds.spawn.SpawnLocation;
 import dev.kyriji.tritonstom.worlds.time.TimeStrategy;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.GameMode;
-import net.minestom.server.entity.Player;
-import net.minestom.server.event.GlobalEventHandler;
-import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
 import net.minestom.server.extras.MojangAuth;
-
-import java.time.Duration;
 
 public class MissileWars {
 	public static TritonWorld world;
+	public static TritonWorld world2;
 
 	public static void main(String[] args) {
-		MinecraftServer minecraftServer = MinecraftServer.init();
-
-		// InstanceManager instanceManager = MinecraftServer.getInstanceManager();
-		// InstanceContainer instanceContainer = instanceManager.createInstanceContainer();
-		// world = instanceContainer;
-		//
-		// instanceContainer.setChunkLoader(new AnvilLoader("data/worlds/world"));
-		// instanceContainer.setGenerator(unit -> unit.modifier().fillHeight(0, 1, Block.GRASS_BLOCK));
-		// instanceContainer.setChunkSupplier(LightingChunk::new);
+		MinecraftServer server = MinecraftServer.init();
 
 		world = WorldManager.get().buildWorld("world")
 				.timeKeeper(time -> time.strategy(TimeStrategy.ALWAYS_NOON))
 				.build();
 
-		GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
-		globalEventHandler.addListener(AsyncPlayerConfigurationEvent.class, event -> {
-			final Player player = event.getPlayer();
-			event.setSpawningInstance(world.getInstanceContainer());
-			player.setRespawnPoint(new Pos(-100.5, 70, 0.5));
-			MinecraftServer.getSchedulerManager().buildTask(() -> {
-						player.setFlying(true);
-						player.setGameMode(GameMode.CREATIVE);
-					})
-					.delay(Duration.ofSeconds(1))
-					.schedule();
-		});
+		world2 = WorldManager.get().buildWorld("world2")
+				.timeKeeper(time -> time.strategy(TimeStrategy.ALWAYS_NOON))
+				.build();
+
+		TritonStom.builder(server)
+				.defaultGameMode(GameMode.CREATIVE)
+				.playerSpawner(spawner -> spawner.fixed(new SpawnLocation(world, new Pos(-100.5, 70, 0.5, 90, 0))))
+				.start();
 
 		MinecraftServer.getCommandManager().register(new TestCommand());
 
 		MojangAuth.init();
-		minecraftServer.start("0.0.0.0", 25565);
+		server.start("0.0.0.0", 25565);
 	}
 }
--- src/main/java/dev/kyriji/missilewars/commands/TestCommand.java
+++ src/main/java/dev/kyriji/missilewars/commands/TestCommand.java
@@ -4,6 +4,7 @@ import dev.kyriji.missilewars.MissileWars;
 import net.minestom.server.command.builder.Command;
 import net.minestom.server.command.builder.arguments.ArgumentType;
 import net.minestom.server.command.builder.arguments.number.ArgumentInteger;
+import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.Player;
 
 public class TestCommand extends Command {
@@ -13,8 +14,14 @@ public class TestCommand extends Command {
 
 		setDefaultExecutor((sender, context) -> {
 			if (!(sender instanceof Player player)) return;
-			player.sendMessage("test " + MissileWars.world.getInstanceContainer().getTimeRate() + " " +
-					MissileWars.world.getInstanceContainer().getTime());
+			player.sendMessage("test " + MissileWars.world.getInstance().getTimeRate() + " " +
+					MissileWars.world.getInstance().getTime());
+
+			player.sendMessage("world1: " + MissileWars.world.getInstance().getUniqueId());
+			player.sendMessage("world2: " + MissileWars.world2.getInstance().getUniqueId());
+			player.sendMessage("player: " + player.getInstance().getUniqueId());
+
+			player.setInstance(MissileWars.world2.getInstance(), new Pos(0, 2, 0));
 
 			// MinecraftServer.getInstanceManager().getInstances().forEach(instance -> {
 			// 	player.sendMessage("saving the world: " + instance.getUniqueId());
@@ -26,7 +33,7 @@ public class TestCommand extends Command {
 
 		addSyntax((sender, context) -> {
 			int number = context.get(numberArgument);
-			MissileWars.world.getInstanceContainer().setTimeRate(number);
+			MissileWars.world.getInstance().setTimeRate(number);
 		}, numberArgument);
 	}
 }

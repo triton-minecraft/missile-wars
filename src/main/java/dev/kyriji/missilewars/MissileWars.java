13e563ae4d224a3a3b9594084e720100278a31fa
Timothy <timothyrwashburn@gmail.com>
2024-12-28 20:30:26 -0800

    update lib implementation

--- src/main/java/dev/kyriji/missilewars/MissileWars.java
+++ src/main/java/dev/kyriji/missilewars/MissileWars.java
@@ -5,6 +5,8 @@ import dev.kyriji.tritonstom.TritonStom;
 import dev.kyriji.tritonstom.worlds.TritonWorld;
 import dev.kyriji.tritonstom.worlds.WorldManager;
 import dev.kyriji.tritonstom.worlds.spawn.SpawnLocation;
+import dev.kyriji.tritonstom.worlds.spawn.SpawnManager;
+import dev.kyriji.tritonstom.worlds.time.TimeManager;
 import dev.kyriji.tritonstom.worlds.time.TimeStrategy;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Pos;
@@ -19,16 +21,17 @@ public class MissileWars {
 		MinecraftServer server = MinecraftServer.init();
 
 		world = WorldManager.get().buildWorld("world")
-				.timeKeeper(time -> time.strategy(TimeStrategy.ALWAYS_NOON))
+				.timeKeeper(TimeManager.get().buildTimeKeeper().strategy(TimeStrategy.ALWAYS_NOON))
 				.build();
 
 		world2 = WorldManager.get().buildWorld("world2")
-				.timeKeeper(time -> time.strategy(TimeStrategy.ALWAYS_NOON))
+				.timeKeeper(TimeManager.get().buildTimeKeeper().strategy(TimeStrategy.ALWAYS_NOON))
 				.build();
 
 		TritonStom.builder(server)
 				.defaultGameMode(GameMode.CREATIVE)
-				.playerSpawner(spawner -> spawner.fixed(new SpawnLocation(world, new Pos(-100.5, 70, 0.5, 90, 0))))
+				.playerSpawner(SpawnManager.get().buildPlayerSpawner()
+						.fixed(new SpawnLocation(world, new Pos(-100.5, 70, 0.5, 90, 0))))
 				.start();
 
 		MinecraftServer.getCommandManager().register(new TestCommand());
